57614a00d1e75119bad9fb6cd14f56d21477f934
Luke Voß <luke@familievoss.net>
2024-03-16 14:49:54 +0100

    created superresolution datasets

--- Project/datasets/__init__.py
+++ Project/datasets/__init__.py
@@ -1,9 +1,11 @@
 from .custom_mnist import CustomMNIST
 from .encoded_mnist import EncodedMNIST, DynamicEncodedMNIST
 from .masked_mnist import EncodedMaskedMNIST, MaskedMNIST
+from .superresolution_mnist import SuperresolutionMNIST
 
 __all__ = ['CustomMNIST',
            'EncodedMNIST',
            'DynamicEncodedMNIST',
            'EncodedMaskedMNIST',
-           'MaskedMNIST']
+           'MaskedMNIST',
+           'SuperresolutionMNIST']
---
+++ Project/datasets/superresolution_mnist.py
@@ -0,0 +1,67 @@
+import torch
+import matplotlib.pyplot as plt
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import Dataset
+
+
+class SuperresolutionMNIST(Dataset):
+    def __init__(self, root="./datasets", train=True):
+        """
+        Initialize normalized MNIST dataset in Tensor format.
+
+        Parameter:
+            root (str): Root directory of dataset where ``MNIST/processed/training.pt``
+                        and  ``MNIST/processed/test.pt`` will be saved.
+            train (bool):   If True, creates dataset from ``training.pt``,
+                            otherwise from ``test.pt``.
+        """
+        reduce_size_transform = transforms.Compose([
+            transforms.Resize((12, 12)),  # Train AE on mini Mnist
+            transforms.ToTensor()
+        ])
+
+        self.mnist = MNIST(root=root, train=train,
+                           transform=transforms.ToTensor(), download=True)
+        self.small_mnist = MNIST(root=root, train=train,
+                                 transform=reduce_size_transform, download=True)
+
+    def __len__(self):
+        """
+        Return the number of samples in dataset.
+        """
+        return len(self.mnist)
+
+    def __getitem__(self, idx):
+        image, label = self.mnist[idx]
+        small_image, _ = self.small_mnist[idx]
+        return image, small_image, label
+
+
+class EncodedSuperresolutionMNIST(Dataset):
+    def __init__(self, autoencoder_small, autoencoder_big, root="./datasets", train=True):
+        self.autoencoder_small = autoencoder_small
+        self.autoencoder_big = autoencoder_big
+        self.autoencoder_small.eval()
+        self.autoencoder_big.eval()
+        self.superresolution_mnist = SuperresolutionMNIST(
+            root=root, train=train)
+
+    def __len__(self):
+        """
+        Return the number of samples in dataset.
+        """
+        return len(self.masked_mnist)
+
+    def __getitem__(self, idx):
+        image, small_image, label = self.superresolution_mnist[idx]
+
+        with torch.no_grad():
+            encoded_small_image = self.autoencoder_small.encode(
+                small_image).detach()
+
+        with torch.no_grad():
+            encoded_image = self.autoencoder_big.encoder(image).detach()
+
+        # Return the small image, original image, and label
+        return encoded_image.squeeze(0), encoded_small_image.squeeze(0), label
