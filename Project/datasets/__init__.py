df72d2ae76f8eec68ff2973feaeff9e15b5feb82
Neelkanth Rawat <42956384+neelkanthrawat@users.noreply.github.com>
2024-03-16 19:34:41 +0100

    upload init for datasets

--- Project/datasets/__init__.py
+++ Project/datasets/__init__.py
@@ -1,9 +1,12 @@
 from .custom_mnist import CustomMNIST
 from .encoded_mnist import EncodedMNIST, DynamicEncodedMNIST
 from .masked_mnist import EncodedMaskedMNIST, MaskedMNIST
+from .custom_encoding_style_transfer import MixedMNIST, CustomEncodedStyleTransfer
 
 __all__ = ['CustomMNIST',
            'EncodedMNIST',
            'DynamicEncodedMNIST',
            'EncodedMaskedMNIST',
-           'MaskedMNIST']
+           'MaskedMNIST',
+           'MixedMNIST',
+           'CustomEncodedStyleTransfer']
