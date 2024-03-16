46f91fc1e5a0a9739f9aa68ce012d299061c5fc5
Neelkanth Rawat <42956384+neelkanthrawat@users.noreply.github.com>
2024-03-16 19:36:05 +0100

    add autoencoder ST and update init autoencoder

--- Project/autoencoder/__init__.py
+++ Project/autoencoder/__init__.py
@@ -1,3 +1,4 @@
 from .autoencoder_simple import AutoencoderSimple
+from .autoencoder_style_transfer import AEStyleTransfer
 
-__all__ = ['AutoencoderSimple']
+__all__ = ['AutoencoderSimple', 'AEStyleTransfer']
